ba5522a9f6a88f5f8986ec731647c3b3f0340ac8
Srellica <faridklerek@gmail.com>
2025-11-24 23:19:38 +0700

    Add ListCategoriesController for fetching all categories

--- lib/app/modules/client/users/pages/1_Home/controllers/list_categories_controller.dart
+++ lib/app/modules/client/users/pages/1_Home/controllers/list_categories_controller.dart
@@ -1,8 +1,13 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:get/get.dart';
 
+// Controller untuk mengambil dan menampilkan semua kategori buku.
 class ListCategoriesController extends GetxController {
+  
+  // Menyimpan daftar kategori yang diambil dari Firestore.
   var categories = <Map<String, dynamic>>[].obs;
+
+  // Status loading saat mengambil data kategori.
   var isLoading = true.obs;
 
   @override
@@ -10,7 +15,7 @@ class ListCategoriesController extends GetxController {
     super.onInit();
     fetchCategories();
   }
-
+  // Mengambil semua kategori dari Firestore (urutan berdasarkan posisi).
   void fetchCategories() async {
     try {
       isLoading.value = true;
