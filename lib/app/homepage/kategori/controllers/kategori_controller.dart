786320cbf91fbbc89633f6224013c06640e481ae
rakarajinibadah <rakabima350@gmail.com>
2025-11-26 00:30:16 +0700

    kategori

---
+++ lib/app/homepage/kategori/controllers/kategori_controller.dart
@@ -0,0 +1,9 @@
+import 'package:get/get.dart';
+
+class KategoriController extends GetxController {
+  //TODO: Implement KategoriController
+
+  final count = 0.obs;
+
+  void increment() => count.value++;
+}
