2dc0643559be1f99d267e02b040733f2197dab37
rakarajinibadah <rakabima350@gmail.com>
2025-12-17 23:06:06 +0700

    book_detail

--- lib/app/homepage/book_detail/bindings/book_detail_binding.dart
+++ lib/app/homepage/book_detail/bindings/book_detail_binding.dart
@@ -1,12 +1,7 @@
 import 'package:get/get.dart';
-
 import '../controllers/book_detail_controller.dart';
 
 class BookDetailBinding extends Bindings {
   @override
-  void dependencies() {
-    Get.lazyPut<BookDetailController>(
-      () => BookDetailController(),
-    );
-  }
+  void dependencies() => Get.lazyPut(() => BookDetailController());
 }
