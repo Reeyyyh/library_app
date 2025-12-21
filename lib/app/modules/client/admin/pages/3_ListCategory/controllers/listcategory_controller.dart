42d66b48930533e431011af459e7bb3047c43ae6
Srellica <faridklerek@gmail.com>
2025-12-21 23:21:28 +0700

    Add category controller with Firestore ordering and reordering

--- lib/app/modules/client/admin/pages/3_ListCategory/controllers/listcategory_controller.dart
+++ lib/app/modules/client/admin/pages/3_ListCategory/controllers/listcategory_controller.dart
@@ -1,6 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:get/get.dart';
-
+// Controller untuk mengelola daftar kategori
 class ListCategoryController extends GetxController {
   final FirebaseFirestore firestore = FirebaseFirestore.instance;
 
