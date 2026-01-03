279fadd423d26150a685cfc7f89a7ffb8ef28356
Srellica <faridklerek@gmail.com>
2026-01-03 23:41:05 +0700

    Add custom application theme

--- lib/app/modules/config/custom_app_theme.dart
+++ lib/app/modules/config/custom_app_theme.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-
+// Kelas yang berisi tema kustom untuk aplikasi perpustakaan
 class CustomAppTheme {
   // Warna utama aplikasi
   static const Color primaryColor = Color(0xFF009140);
